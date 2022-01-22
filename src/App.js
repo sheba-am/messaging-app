9520d44e3939d75fecddeb2ffc06d9c41a8c4f08
sheba <68592737+sheba-am@users.noreply.github.com>
2022-01-23 00:28:37 +0330

    add login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import React, { Component } from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 import Main from './components/MainComponent'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Login from './components/LoginComponent' ;
+import useLocalStorage from "./hooks/useLocalStorage";
+// class App extends Component {
+//   state = {
+//     isLoggedIn: true,
+//     messages: [],
+//     value: '',
+//     name: '',
+//     room: 'vacad',
+//   }
 
-class App extends Component {
+//   client = new W3CWebSocket('ws://django-chat-app.herokuapp.com/ws/chat/' + this.state.room + '/');
+//   componentDidMount() {
+//     this.client.onopen = () => {
+//     console.log('WebSocket Client Connected');
+//       }
+//     }
+
+//   render() {
+//     return (
 
 
-
-  render() {
-    return (
-
-
-          <div className="App">
-            <Main />
-          </div>
+//           <div className="App">
+//             <Main />
+//           </div>
  
 
-    )
-  }
-}
+//     )
+//   }
+// }
+function App() {
+  const[id,setId] =useLocalStorage('id'); //pass the id value to stay with us
+    
+    return (
 
+        id ? <Main id={id} /> :<Login onIdSubmit={setId} />
+
+    )
+}
 export default App;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -21,4 +21,5 @@ class Main extends Component{
         );
     }
 }
+
 export default Main;
