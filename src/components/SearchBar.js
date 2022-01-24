2e9c10bb7cb903ae1d1b0ccabb32fd6edde18820
sheba <68592737+sheba-am@users.noreply.github.com>
2022-01-25 01:12:41 +0330

    add backend to search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@
    
 import React, { useState } from "react";
 import {search} from 'bootstrap-icons/font/bootstrap-icons.css';
-
+import axios from 'axios'
 //import "./SearchBar.css";
 //import SearchIcon from "@material-ui/icons/Search";
 //import CloseIcon from "@material-ui/icons/Close";
@@ -10,19 +10,43 @@ import {search} from 'bootstrap-icons/font/bootstrap-icons.css';
 function SearchBar({ placeholder, data }) {
   const [filteredData, setFilteredData] = useState([]);
   const [wordEntered, setWordEntered] = useState("");
-
+  const [users, setUsers] = useState([]);
+  
+  const config = {
+    headers: {
+        'Content-type': 'application/json',
+    }
+  }
+  const getResults = async () => {
+    
+    
+  }
   const handleFilter = (event) => {
     const searchWord = event.target.value;
     setWordEntered(searchWord);
-    const newFilter = data.filter((value) => {
-      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+
+    const result = axios.post(
+      'http://127.0.0.1:8000/api/users/search',
+      {"keyword":wordEntered}
+      , config
+    ).then((response) => response)
+    .then((response) => {
+      console.log(response.data[0].username)
+      setFilteredData(response.data)
+    })
+    console.log("users are:")
+    console.log(filteredData)
+    const newFilter = users.filter((value) => {
+      console.log("value is:" + value.username)
+      return value.username.toLowerCase()
     });
 
     if (searchWord === "") {
       setFilteredData([]);
-    } else {
-      setFilteredData(newFilter);
     }
+    // } else {
+    //   setFilteredData(newFilter);
+    // }
   };
 
   const clearInput = () => {
@@ -47,7 +71,7 @@ function SearchBar({ placeholder, data }) {
           {filteredData.slice(0, 15).map((value, key) => {
             return (
               <a className="dataItem" href={value.link} target="_blank">
-                <p>{value.title} </p>
+                <p>{value.username} </p>
               </a>
             );
           })}
